cc65779871831442ded6148add236ca150184c63
肖钢 <gawain.xiao@xiaogangdeMacBook-Pro.local>
2019-04-07 21:58:58 +0800

    vue组件学习之路开始

---
+++ src/components/form/form-item.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <label for="">{{label}}</label>
+    <div>
+        <slot></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:'iFormItem',
+    data(){
+
+    },
+    props:{
+        label:{
+            type:String,
+            default:''
+        },
+        prop:{
+            type:String
+        }
+    }
+}
+
+</script>
+<style>
+</style>
