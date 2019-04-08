ce6ffc47b2e2183428661f3c12c210be3647b65a
肖钢 <gawain.xiao@xiaogangdeMacBook-Pro.local>
2019-04-08 22:55:58 +0800

    深入理解dispath和broadcast的妙用

--- src/components/form/form-item.vue
+++ src/components/form/form-item.vue
@@ -8,11 +8,13 @@
 </template>
 
 <script>
+import emit from '../../utils/emit.js'
 export default {
     name:'iFormItem',
     data(){
 
     },
+    mixins:[emit],
     props:{
         label:{
             type:String,
@@ -21,6 +23,16 @@ export default {
         prop:{
             type:String
         }
+    },
+    mounted(){
+        //如果没有传入prop,则无需验证，更不需要派发事件到from组件缓存
+        if(this.prop){
+            this.dispatch('iForm','form-item-add',this);
+        }
+    },
+    //组件销毁前，将缓存从form组件清除掉
+    beforeDestroy(){
+        this.dispatch('iForm','form-iten-remove',this);
     }
 }
 
---
+++ src/components/form/form.vue
@@ -0,0 +1,39 @@
+<template>
+    <form action="">
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+export default {
+    name:"iForm",
+    data(){
+        items:[]
+    },
+    props:{
+        model:{
+            type:Object
+        },
+        rules:{
+            type:Object
+        }
+    },
+    created(){
+        this.$on('form-item-add',item =>{
+            if(item){
+                this.items.push(item);
+            }
+        });
+
+        this.on('form-item-remove',item=>{
+            if(item.prop){
+                this.items.splice(this.items.indexof(item),1)
+            }
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
